ac455cf0aa36c7276cafb81c1f778d473f9fb5e9
0RAPTOR0 <xxiiraptor@gmail.com>
2024-05-31 20:09:28 +0530

    react jsx

---
+++ src/app/aboutus/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const aboutus = () => {
+    return (
+        <div className='text-5xl'>About Us</div>
+    )
+}
+
+export default aboutus
---
+++ src/app/signup/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Signup = () => {
+    return (
+        <div className='text-4xl'>Signup Page</div>
+    )
+}
+
+export default Signup;
